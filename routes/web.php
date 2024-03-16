aca838d59ced3237a98ecd0b71e1c61ff46793e9
Ranagol <andorhorvat@gmail.com>
2024-03-16 15:50:48 +0100

    Change database collation - otherwise db not working.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//This route returns a JSON response
+Route::get('/json-test', function () {
+    return response()->json([
+        'name' => 'Jone',
+        'updated' => true,
+    ]);
+});
+
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -9,6 +9,9 @@ class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
+     * In this example, a GET request is sent to the root URL, and the returned response is assigned
+     * to the $response variable. On the following line, we check if the response has a success
+     * code of 200 with the assertStatus() assertion.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
